c2fe596a72e63b7f82d17a76e225d2c4feaba7cc
Nathaniel <ultimote01@hotmail.com>
2026-05-06 12:22:44 -0700

    login page finished

--- src/components/LoginLayout.jsx
+++ src/components/LoginLayout.jsx
@@ -1,11 +1,9 @@
 export default function LoginLayout(){
  
 return(
-  <section className="h-[100%] flex flex-col justify-between">
+  <section className="h-[100%] flex flex-col justify-between gap-y-16">
     <div className="flex flex-col ">
-        <div className="pb-[6rem] flex justify-start">
-        </div>
-        <div className="flex flex-col justify-center items-center px-3">
+        <div className="mt-16 flex flex-col justify-center items-center px-3">
         <div className="flex bg-[var(--form-bg)] flex-col px-8 py-14 w-[100%] rounded-xl max-w-[30rem] border-[0.4px] border-solid border-zinc-200 dark:border-[0px] lg:px-16"
         style={{boxShadow: `var(--shadow)`}}
         >
@@ -21,12 +19,12 @@ return(
                   <div className="flex flex-col mt-[1.5rem]">
                     <label htmlFor="email" className="text-[1rem] dark:text-white">Email</label>
                     <input className="py-2 pl-2 border-[2.8px] border-solid  border-zinc-200 mt-[0.4rem] rounded-[5px] dark:bg-white dark:border-gray-600 dark:text-zinc-600"
-                     type="text" name="email" />
+                     type="text" name="email" placeholder="jhondoe@example.com" />
                   </div>
                   <div className="flex flex-col mt-[1.5rem]">
                       <label htmlFor="password" className="text-[1rem] dark:text-white">Password</label>
                       <input  className="py-2 pl-2 border-[2.8px] border-solid  border-zinc-200 mt-[0.4rem]  mt-[0.4rem] rounded-[5px] dark:bg-white dark:border-gray-600 dark:text-zinc-600" 
-                      type="text" name="password"    />
+                      type="text" name="password"  placeholder="********"  />
                   </div>
 
                   <div className="flex mt-12 lg:justify-center">
@@ -39,7 +37,7 @@ return(
         </div>
     </div>
    
-    <div className="text-balck mt-2    text-[1rem] leading-[1.5rem] dark:text-white lg:mt-12">
+    <div className="text-balck  text-[1rem] leading-[1.5rem] dark:text-white ">
           <div className="  flex justify-center text-center py-4 mx-4  md:mx-16">
           <span className=" flex gap-x-1.5 md:gap-x-4"> &copy; 2026 Yabatech. All rights reserved<i></i>
           <a href="/login">Terms of use</a>
